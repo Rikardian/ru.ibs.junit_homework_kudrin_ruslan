05bc3d59bcc0d890e212c247d8ef048ae5a254e2
Rikardian <92721408+Rikardian@users.noreply.github.com>
2021-10-29 18:20:36 +0400

    Выполнил первую часть домашнего задания без тестов

---
+++ src/main/java/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        String fileName = "";
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            fileName = reader.readLine();
+        }catch (IOException e){
+            System.out.println("Неправильное имя файла");
+        }
+
+        Solution solution = new Solution();
+
+        solution.fizzBuzzer(fileName);
+    }
+
+    public List<String> readFile(String fileName){
+        List<String> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            while (reader.ready()){
+                result.add(reader.readLine());
+            }
+            return result;
+        } catch (FileNotFoundException e){
+            System.out.println("Файл не найден!");
+        } catch (IOException e){
+            System.out.println("Введены невалидные данные");
+        }
+        return null;
+    }
+
+    public List<String> numberChanger (List<String> list){
+        List<String> result = new ArrayList<>();
+
+        for (String value : list){
+            int num = Integer.parseInt(value);
+            if (num % 5 == 0 && num % 3 == 0){
+                result.add("FizzBuzz");
+            }
+            else if (num % 5 == 0){
+                result.add("Buzz");
+            }
+            else if (num % 3 == 0){
+                result.add("Fizz");
+            }
+            else result.add(value);
+        }
+        return result;
+    }
+
+    public void fizzBuzzer(String fileName){
+        List<String> result = numberChanger(readFile(fileName));
+        result.forEach(System.out::println);
+    }
+}
